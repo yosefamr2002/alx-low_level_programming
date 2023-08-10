625d2b63830ad41fba9a3c26dc2dd1b1a8115acc
Youssef Amr <yosefamr2002@icloud.com>
2023-08-10 15:53:54 -0700

    auto commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
-#incloude <stdio.h>
-int main(){
+#include <stdio.h>
 
-puts("\"Programming is like building a multilingual puzzle")
-
-return(0);
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+		return(0);
 }
