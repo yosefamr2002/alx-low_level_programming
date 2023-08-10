6c9bfcc8bfdde13517bd3f2fb3f13942256c7f2f
Youssef Amr <115193906+yosefamr2002@users.noreply.github.com>
2023-08-11 02:15:16 +0300

    Update 4-puts.c
    gi commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
-*main - Entry point
+* main - Entry point
 *
-*Description: A C program that prints with put function
+* Description: A C program that prints with put function
 *
-*Return: Alawys 0 (Success)
+* Return: Alawys 0 (Success)
 */
 
 int main(void)
