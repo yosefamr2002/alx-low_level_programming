336b5c3b60807513a066a34c4c7006b802d667a8
Youssef Amr <yosefamr2002@icloud.com>
2023-08-10 16:41:56 -0700

    auto commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  *main - Entry point
  *
@@ -6,11 +7,12 @@
  *
  *Return: Always 0 (Success)
 */
+
 int main(void)
 {
-	printf("size of a char: %lu byte(s)\n", sizeof (char));
+	printf("size of a char: %u byte(s)\n", sizeof(char));
 	printf("size of an int: %u byte(s)\n", sizeof(int));
-	printf("size of a long int: %u byte(s)\n", sizeof (long int));
+	printf("size of a long int: %u byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %u byte(s)\n", sizeof(long long int));
 	printf("size of a float: %u byte(s)\n", sizeof(float));
 	return (0);
