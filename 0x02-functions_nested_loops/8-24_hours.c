2271a6dd25f666200d612200e914d78ccca1bea1
Youssef Amr <yosefamr2002@icloud.com>
2023-08-16 03:02:05 -0700

    commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,24 +1,28 @@
 #include "main.h"
 
 /**
-* jack_bauer - prints every minute of the day of Jack Bauer
-*/
-
+  * jack_bauer - prints every minute of the day of Jack Bauer
+  * starting from 00:00 to 23:59
+  */
 void jack_bauer(void)
 {
+	int i, j;
 
-	int min, hr;
+	i = 0;
 
-	for (hr = 0; hr <= 23; hr++)
+	while (i < 24)
 	{
-		for (min = 0; min < 59; min++)
+		j = 0;
+		while (j < 60)
 		{
-			_putchar((hr / 10) + 48);
-			_putchar ((hr % 10) + 48);
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
 			_putchar(':');
-			_putchar((min / 10) + 48);
-			_putchar((min % 10) + 48);
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
 			_putchar('\n');
+			j++;
 		}
+		i++;
 	}
 }
